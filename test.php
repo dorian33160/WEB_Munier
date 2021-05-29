cb6932d3ccb943f757bd3021e25ce11a27f4fa37
dorian33160 <dorianneilz33@gmail.com>
2021-05-29 11:11:49 +0200

    k

--- test.php
+++ test.php
@@ -1,20 +1,28 @@
 <html>
+
 <head>
  <title>Premier exemple php sqlite</title>
 </head>
+
 <body>
+
 <?php
 $db = new SQLite3('jeux.db');
 
 $results = $db->query('SELECT * FROM infosJeu');
 ?>
+
 <h3> Liste des jeux</h3>
+
 <ul>
+
 <?php
 while ($row = $results->fetchArray()) {
  echo "<li>{$row['infosJeu']}-{$row['infosEditeur']}-{$row['infosClient']}-{$row['infosPLateforme']}-{$row['infosPayement']}-{$row['panier']}-{$row['achat']} </li>";
 }
 ?>
+
 </ul>
+
 </body>
 </html>
