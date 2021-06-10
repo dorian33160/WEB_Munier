70457ba61a075b0d554fe8c09edd4ea65a3b2165
dorian33160 <84706476+dorian33160@users.noreply.github.com>
2021-06-10 09:46:57 +0200

    pages jeux

--- 4.php
+++ 4.php
@@ -8,21 +8,58 @@
 
 <body>
 
-<?php
-$db = new SQLite3('jeux.db');
+  <?php
+  try{
+   $pdo = new PDO('sqlite:jeux.db');
+   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
+   echo "Connecté à la base SQLite jeux.db<br>";
 
-$results = $db->query("SELECT * FROM infosJeu WHERE Nom='Resident Evil Village'");
-?>
+   // Création d'une requête SQL sur une table
+   $stmt = $pdo->prepare("SELECT * FROM infosJeu WHERE Nom='Resident Evil Village'");
+   $stmt->execute();
+   //$result = $stmt->fetchAll();
+   //print_r($result);
+   ?>
+   <h3>Resident Evil Village</h3>
+   <table>
+   <thead>
+   <tr>
+   <th>N°</th>
+   <th>Nom Jeu</th>
+   <th>Prix Jeu</th>
+   <th>Date Sorite</th>
+   <th>Style</th>
+   <th>Editeur</th>
+   <th>PEGI</th>
+   <th>Langue</th>
+   <th>N° plateforme</th>
+   </tr>
 
-<h3>Resident Evil Village</h3>
+   </thead>
 
-<ul>
-<?php
-while ($row = $results->fetchArray()) {
- echo "<li>{$row['Nom']}</li><li>{$row['Prix']}</li><li>{$row['DateSortie']}</li><li>{$row['Style']}</li><li>{$row['Editeur']}</li><li>{$row['PEGI']}</li><li>{$row['Langue']} </li>";
-}
-?>
-</ul>
+   <?php
+   while ($row = $stmt->fetchObject()) {
+   echo "<tr>
+   <td>{$row->NumJeu}</td>
+   <td>{$row->Nom}</td>
+   <td>{$row->Prix}</td>
+   <td>{$row->DateSortie}</td>
+   <td>{$row->Style}</td>
+   <td>{$row->Editeur}</td>
+   <td>{$row->PEGI}</td>
+   <td>{$row->Langue}</td>
+   <td>{$row->NumPlateforme}</td>
+   </tr>";
+   }
+   ?>
+   </table>
+  <?php
+  } catch(Exception $e) {
+   echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
+   die();
+  }
+  ?>
 
 </body>
 </html>
